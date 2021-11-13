a9ecfbb57caa715541d857cb126d995316073eb5
vjgaur <vijayendra.gaur@gmail.com>
2021-11-13 14:57:52 +0530

    variables and data types in rust

--- src/print.rs
+++ src/print.rs
@@ -1,23 +1,29 @@
-pub fn run(){
-    //Print to console
+pub fn run() {
+    // Print to console
     println!("Hello from the print.rs file");
-
-    //Basic formating 
-    println!("{} is from {}","Vij","Mars");
-
-    //Positional Arguments 
-    println!("{0} is from {1} and {0} likes to {2}", "Vij","Mars","Blockchain");
-
-    //Named Arguments 
-    println!("{name} likes to play {activity}", name="Vij", activity="Cricket");
-
-    //Placeholder traits 
-    println!("Binary: {:b} Hex: {:x} Octal: {:o}",10,10,10);
-
-    //Placeholder for debug trait 
-    println!("{:?}",(12, true, "hello"));
-
-    //Basic Math
-    println!("10 + 10={}", 10+10);
-
-}
+  
+    // Basic Formatting
+    println!("{} is from {}", "Brad", "Mass");
+  
+    // Positional Arguments
+    println!(
+      "{0} is from {1} and {0} likes to {2}",
+      "Brad", "Mass", "code"
+    );
+  
+    // Named Arguments
+    println!(
+      "{name} likes to play {activity}",
+      name = "John",
+      activity = "Baseball"
+    );
+  
+    // Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+  
+    // Placeholder for debug trait
+    println!("{:?}", (12, true, "hello"));
+  
+    // Basic math
+    println!("10 + 10 = {}", 10 + 10);
+  }
